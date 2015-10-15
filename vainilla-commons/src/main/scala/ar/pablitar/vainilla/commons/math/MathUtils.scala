55d906a383ced662cd96fc9750bd8c6a1ecc5558
Pablo de Haro <pablitar@gmail.com>
2015-10-14 22:48:32 -0300

    Sacado Vainilla Commons. Va a ir a otro proyecto

--- vainilla-commons/src/main/scala/ar/pablitar/vainilla/commons/math/MathUtils.scala
+++
@@ -1,8 +0,0 @@
-package ar.pablitar.vainilla.commons.math
-
-object MathUtils {
-  def bound(aNumber: Double, lowerBound: Double, upperBound: Double) = 
-    aNumber max lowerBound min upperBound
-
-  implicit def double2Int(double: Double): Int = double.toInt
-}
--- vainilla-commons/src/main/scala/ar/pablitar/vainilla/commons/math/PhysicsUtils.scala
+++
@@ -1,8 +0,0 @@
-package ar.pablitar.vainilla.commons.math
-
-/**
- * @author pablitar
- */
-object PhysicsUtils {
-  def rebound(v: Vector2D, n: Vector2D) = (-2 * v.proyectTo(n)) + v
-}
